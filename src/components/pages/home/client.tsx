a2fca37fd425a24f00fd2bbaccead6578c5529a5
imopbuilder <opbuilder9@gmail.com>
2024-01-07 20:05:07 +0530

    styles and fix: add styles to the quote component and add attribute to html tag to suppressHydrationWarning

--- src/components/pages/home/client.tsx
+++ src/components/pages/home/client.tsx
@@ -1,20 +1,25 @@
 'use client';
 
+import { useAppSelector } from '@/client/store';
+
 export function Quote() {
+	const { quote } = useAppSelector((state) => state.quoteSlice);
+	const { content, author, tags } = quote;
+
 	return (
 		<div>
-			<p className='py-10'>Slow and steady wins the race</p>
+			<p className='py-20 text-7xl/[1.4] font-bold bg-gradient-to-br from-foreground to-muted-foreground bg-clip-text text-transparent'>{content}</p>
 			<hr />
-			<div>
-				<p>tag</p>
+			<div className='flex items-center justify-between py-5 text-sm'>
 				<div>
-					<div>
-						<p>Image</p>
-					</div>
-					<div>
-						<p>author name</p>
-						<p>profession</p>
-					</div>
+					{tags.map((tag) => (
+						<span key={tag} className='uppercase text-xs text-muted-foreground border rounded-lg py-1.5 px-2'>
+							{tag}
+						</span>
+					))}
+				</div>
+				<div>
+					<p className='ext-muted-foreground font-semibold'>- {author}</p>
 				</div>
 			</div>
 		</div>
