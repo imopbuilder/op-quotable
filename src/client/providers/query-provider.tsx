d68aebae5b9a90b9f1f5d8f97cc8256dae03f94c
imopbuilder <opbuilder9@gmail.com>
2024-01-08 13:49:22 +0530

    enhace: prevent refetch on windowFocus

--- src/client/providers/query-provider.tsx
+++ src/client/providers/query-provider.tsx
@@ -4,7 +4,13 @@ import { ReactNode } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 // Create a client
-const queryClient = new QueryClient();
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+});
 
 export function QueryProvider({ children }: { children: ReactNode }) {
 	return <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>;
